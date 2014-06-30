b367f8ec2c3c2d854616c66c4e02d26d56d3cafd
charliieuy <carlosuy@carloss-mbp.home>
2014-06-30 00:44:02 -0400

    Initial Commit

---
+++ Cards/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cards
+//
+//  Created by Carlos Uy on 6/30/14.
+//  Copyright (c) 2014 Orange Box Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cards/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Cards
+//
+//  Created by Carlos Uy on 6/30/14.
+//  Copyright (c) 2014 Orange Box Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
